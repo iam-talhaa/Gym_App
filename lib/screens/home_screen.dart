ebd9b11ad7bef60015ac5f3813b566fc83e39b44
MUHAMMAD TALHA <tahakhan4141@gmail.com>
2024-04-21 23:58:53 +0500

    add photos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class homescreen extends StatefulWidget {
   const homescreen({super.key});
@@ -16,7 +17,45 @@ class _homescreenState extends State<homescreen> {
         width: double.infinity,
         decoration: BoxDecoration(
             image: DecorationImage(
-                fit: BoxFit.cover, image: AssetImage('lib/images/3.jpg'))),
+                fit: BoxFit.cover, image: AssetImage('lib/images/2.jpg'))),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 70, left: 20),
+                      child: Text('Hey,  ',
+                          style: GoogleFonts.bebasNeue(
+                              letterSpacing: 2,
+                              fontSize: 31,
+                              color: Colors.white)),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 70),
+                        child: Text(' TALHA',
+                            style: GoogleFonts.bebasNeue(
+                                letterSpacing: 2,
+                                fontSize: 31,
+                                color: Colors.green))),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70, right: 20),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.green)),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
