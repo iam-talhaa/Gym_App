22eaf1fa35b1bbe0d2c855b338c6119ad206c4fc
MUHAMMAD TALHA <tahakhan4141@gmail.com>
2024-04-25 23:49:37 +0500

    updated

--- lib/screens/purchase.dart
+++ lib/screens/purchase.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class purchase_screen extends StatefulWidget {
@@ -17,40 +20,53 @@ class _purchase_screenState extends State<purchase_screen> {
         decoration: BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.cover, image: AssetImage('lib/images/p2.jpg'))),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Row(
+        child: Expanded(
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 100, horizontal: 30),
-                      child: Text(
-                        'Membership',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    )
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 100, horizontal: 30),
+                          child: Text(
+                            'Membership',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 100, horizontal: 0),
+                          child: Text(
+                            'PANEL',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 100, horizontal: 30),
-                      child: Text(
-                        'PANEL',
-                        style: TextStyle(fontSize: 20, color: Colors.green),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            Column(
-              children: [],
-            )
-          ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration:
+                        BoxDecoration(color: Colors.white.withOpacity(0.8)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
